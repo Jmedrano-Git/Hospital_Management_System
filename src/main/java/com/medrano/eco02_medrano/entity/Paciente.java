33006f8859c59e831bac6b0395b8b41957fe0d9a
Julio Medrano <julio.medrano@tecsup.edu.pe>
2025-10-18 20:36:26 -0500

    Imports and Creation Entity Paciente.

--- src/main/java/com/medrano/eco02_medrano/entity/Paciente.java
+++ src/main/java/com/medrano/eco02_medrano/entity/Paciente.java
@@ -1,4 +1,12 @@
 package com.medrano.eco02_medrano.entity;
 
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "paciente")
 public class Paciente {
 }
