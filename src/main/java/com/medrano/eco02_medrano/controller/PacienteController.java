f0b74739bf6bbf0157e5efb9c136ceff899ce9fb
Julio Medrano <julio.medrano@tecsup.edu.pe>
2025-10-18 20:40:56 -0500

    Imports.

--- src/main/java/com/medrano/eco02_medrano/controller/PacienteController.java
+++ src/main/java/com/medrano/eco02_medrano/controller/PacienteController.java
@@ -1,4 +1,14 @@
 package com.medrano.eco02_medrano.controller;
 
+import com.medrano.eco02_medrano.entity.Paciente;
+import com.medrano.eco02_medrano.service.PacienteService;
+import jakarta.validation.Valid;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("/pacientes")
 public class PacienteController {
 }
