6265583f44b9c0b5fa12ab8b048726ee3b5b4276
Julio Medrano <julio.medrano@tecsup.edu.pe>
2025-10-18 20:06:55 -0500

    First Commit

---
+++ src/main/java/com/medrano/eco02_medrano/Eco02MedranoApplication.java
@@ -0,0 +1,13 @@
+package com.medrano.eco02_medrano;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Eco02MedranoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Eco02MedranoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/medrano/eco02_medrano/Eco02MedranoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medrano.eco02_medrano;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Eco02MedranoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
