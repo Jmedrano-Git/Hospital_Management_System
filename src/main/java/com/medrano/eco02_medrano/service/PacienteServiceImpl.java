f381e280896cf1071c76e803b81012896f8cddd3
Julio Medrano <julio.medrano@tecsup.edu.pe>
2025-10-18 20:40:25 -0500

    Implementation Methods.

--- src/main/java/com/medrano/eco02_medrano/service/PacienteServiceImpl.java
+++ src/main/java/com/medrano/eco02_medrano/service/PacienteServiceImpl.java
@@ -1,4 +1,40 @@
 package com.medrano.eco02_medrano.service;
 
-public interface PacienteServiceImpl {
+import com.medrano.eco02_medrano.entity.Paciente;
+import com.medrano.eco02_medrano.repository.PacienteRepository;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+public class PacienteServiceImpl implements PacienteService {
+
+    private final PacienteRepository repo;
+
+    public PacienteServiceImpl(PacienteRepository repo) {
+        this.repo = repo;
+    }
+
+    @Override
+    public List<Paciente> listar() {
+        return repo.findAll();
+    }
+
+    @Override
+    public void grabar(Paciente paciente) {
+        repo.save(paciente);
+    }
+
+    @Override
+    public Paciente buscar(Integer id) {
+        return repo.findById(id).orElse(null);
+    }
+
+    @Override
+    public void desactivar(Integer id) {
+        Paciente p = buscar(id);
+        if (p != null) {
+            p.setEstado("inactivo");
+            repo.save(p);
+        }
+    }
 }
